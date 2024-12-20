c28fc38f594bc022e761faf0d73245708c0546a5
RobertoFloris <florisroberto99@gmail.com>
2024-12-20 10:23:29 +0100

    fix es 2

--- ex-2/index.js
+++ ex-2/index.js
@@ -18,3 +18,14 @@ const regali = [
   { nome: "Libro", prezzo: 10 },
   { nome: "Giocattolo", prezzo: 20 }
 ];
+
+console.log(calcolaTotale(regali));
+
+function calcolaTotale(array){
+  let somma = 0;
+  array.forEach(element => {
+    somma += element.prezzo;
+  })
+  return somma;
+}
+
